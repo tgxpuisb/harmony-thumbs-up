87ab7e6ad4e06f448cf9643f5690e38cdc7a1894
yizhi.li <tgxpuisb@hotmail.com>
2017-12-20 23:44:16 +0800

    go on this project

--- README.md
+++ README.md
@@ -15,3 +15,7 @@ https://github.com/heshang951?page=5&tab=stars
 https://github.com/zczhangchao51/fjpublish
 https://github.com/ouzhrm
 https://github.com/zczhangchao51/fjpublish/stargazers
+
+
+## 更新日志
+2017.12.20 本来想着这个项目黄了,不想今天发现了公司的某些同事也在购买这个点赞服务,SO,我决定将这个项目继续下去
--- app/app/spiders/user_detail.py
+++ app/app/spiders/user_detail.py
@@ -1,10 +1,16 @@
 # 用户详细信息
 import scrapy
 from app.items import UserItem
-
-class UserDetail(scrapy.spider):
-
-    name = 'user_detail'
-
-    pass
-
+#
+# class UserDetail(scrapy.spider):
+#
+#     name = 'user_detail'
+#     base_url = 'https://github.com/'
+#     allowed_domains = ['github.com']
+#     start_urls = []
+#
+#     def __init__(self):
+#         super()
+#
+#     pass
+#
